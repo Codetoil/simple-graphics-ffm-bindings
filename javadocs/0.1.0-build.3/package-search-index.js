335b352c8fc89779a730cb801301a6f264edeb21
Codetoil <codetoil@users.noreply.github.com>
2026-03-04 22:34:54 +0000

    Automated update 0.1.0-build.3 (Javadoc)

---
+++ javadocs/0.1.0-build.3/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"m":"glfw_vulkan","l":"glfw_vulkan"},{"m":"glfw","l":"glfw3"},{"m":"vulkan","l":"vulkan"}];updateSearchResults();
